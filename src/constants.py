39a5b3720da3e97316f850a4ef452a4aaa102846
Michael Mekonnen <mikemeko@mit.edu>
2013-04-05 12:25:18 -0400

    slightly better gradient filters

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
 from two_D_signal import Two_D_Signal
 
-HORIZONTAL_EDGE_FILTER = Two_D_Signal({(-1, -1): -1, (0, -1): -1, (1, -1): -1,
-    (-1, 1): 1, (0, 1): 1, (1, 1): 1})
-VERTICAL_EDGE_FILTER = Two_D_Signal({(-1, -1): -1, (-1, 0): -1, (-1, 1): -1,
-    (1, -1): 1, (1, 0): 1, (1, 1): 1})
+HORIZONTAL_EDGE_FILTER = Two_D_Signal({(-1, -1): -1, (0, -1): -2, (1, -1): -1,
+    (-1, 1): 1, (0, 1): 2, (1, 1): 1})
+VERTICAL_EDGE_FILTER = Two_D_Signal({(-1, -1): -1, (-1, 0): -2, (-1, 1): -1,
+    (1, -1): 1, (1, 0): 2, (1, 1): 1})
